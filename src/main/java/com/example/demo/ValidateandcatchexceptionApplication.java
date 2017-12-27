8721842c709fb823fbce36928916f78765e3d42c
lzy <1158600445@qq.com>
2017-12-27 15:06:46 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/ValidateandcatchexceptionApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidateandcatchexceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidateandcatchexceptionApplication.class, args);
+	}
+}
