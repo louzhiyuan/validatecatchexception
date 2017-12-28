6999066e5ba852e510b418daf9c14c65be8cc6a9
louzhiyuan <34015181+louzhiyuan@users.noreply.github.com>
2017-12-28 10:16:09 +0800

    Update DaDeController.java

--- src/main/java/com/example/demo/controller/DaDeController.java
+++ src/main/java/com/example/demo/controller/DaDeController.java
@@ -30,82 +30,18 @@ public class DaDeController {
     private static final Logger logger = LoggerFactory.getLogger(DaDeController.class);
 
     String Edu_result;
-
-    //学历认证 post请求
-    @RequestMapping(value="/getEdu", produces="application/json;charset=UTF-8", method= RequestMethod.POST)
-    public Response getEdu(@RequestBody Request request) {
-        log.info("收到请求： {}", request);
-
-        Response response = new Response();
-
-        String jsonData = request.getJsonData();
-
-        JSONObject jsonObject = JSON.parseObject(jsonData);
-        String idNo = (String) jsonObject.get("idNo");
-        String name = (String) jsonObject.get("name");
-
-
-                Edu_result= getE(idNo,name);
-
-                JSONObject thirdResJson = JSONObject.parseObject(Edu_result);
-                String code = thirdResJson.getString("code");
-                switch(code) {
-                    case "200":
-                        response.setResCode("0000");
-                        response.getData().setStatusCode("2012");
-                        response.getData().setResult(thirdResJson.getJSONObject("data").getJSONArray("value"));
-                        break;
-
-                    case "403":
-                    case "417":
-                    case "418":
-                        //1001没有此接口的访问权限
-                        response.setResCode("1005");
-                        //本数据库未查得2007
-                        response.getData().setStatusCode("2007");
-                        break;
-                    case "400":
-                    case "404":
-                    case "406":
-                    case "411":
-                    case "413":
-                    case "414":
-                        //1005 请求参数未空或者格式错误
-                        response.setResCode("1005");
-                        //2006 不一致
-                        response.getData().setStatusCode("2016");
-                        break;
-                    case "412":
-                    case "10011":
-                    case "10021":
-                    case "10007":
-                    case "10008":
-                    case "10009":
-                        //1005 请求参数为空或者格式错误
-                        response.setResCode("1005");
-                        //2013系统内部错误
-                        response.getData().setStatusCode("2013");
-                        break;
-                    default:
-                        //1001没有此接口的访问权限
-                        response.setResCode("1001");
-                        //2013系统内部错误
-                        response.getData().setStatusCode("2013");
-                        break;
-                }
-        //日志响应
-        log.info("响应数据:{}", response);
-        return response;
-    }
-
-
-    //大德对接get请求
+    
+    //get请求
     @RequestMapping(value="/checkEducation", produces="application/json;charset=UTF-8", method= RequestMethod.GET)
-    public Response getE2(@Size(min = 15,max = 18,message = "身份证号码必须是15位或18位") @RequestParam("idNo")String idNo,
-                          @NotBlank(message = "姓名不能为空")  @RequestParam("name")String name){
+                           //@Size判断字符长度
+                           //@NotBlank判断字符串是否为空 @NotEmpty @NotNull
+                           //@Max int类型的判断
+                           //返回的字符串必须是jsonObject类型
+    public Response getE2(@Size(min = 15,max = 18,message = "{'resCode':'1005','resMsg':'身份证号码必须是15位或18位'}") @RequestParam("idNo")String idNo,
+                          @NotBlank(message = "{'resCode':'1005','resMsg':'姓名不能为空'}")  @RequestParam("name")String name){
 
         Response response = new Response();
-        //把姓名过滤
+        //把姓名过滤，自动排除空格
         String data = getE(idNo,name.replace(" ",""));
         JSONObject thirdResJson = JSONObject.parseObject(data);
         System.out.println("学历:"+thirdResJson.toString());
@@ -124,17 +60,6 @@ public class DaDeController {
                 response.setResCode("1005");
                 response.getData().setStatusCode("2012");
                 break;
-            case "401":
-            case "402":
-            case "403":
-            case "404":
-            case "405":
-            case "406":
-            case "416":
-                //1001没有此接口的访问权限
-                response.setResCode("1001");
-                response.getData().setStatusCode("2012");
-                break;
             case "411":
             case "412":
             case "413":
@@ -142,14 +67,6 @@ public class DaDeController {
                 //1001没有此接口的访问权限
                 response.setResCode("1001");
                 response.getData().setStatusCode("2006");
-                break;
-            case "415":
-                response.setResCode("0000");
-                response.getData().setStatusCode("2007");
-                break;
-            case "511"://暂时按照超时处理
-                response.setResCode("0000");
-                response.getData().setStatusCode("2013");
                 break;
             default:
                 response.setResCode("0000");
@@ -173,8 +90,9 @@ public class DaDeController {
         return Edu_result;
     }
 
+    
 
-    //异常处理
+    //异常处理，捕获validate验证失败的异常，返回异常的信息解析
     @ExceptionHandler(value = { ConstraintViolationException.class })
     @ResponseBody
     public String handleResourceNotFoundException(ConstraintViolationException e) {
