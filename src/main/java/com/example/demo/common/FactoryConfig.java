5691c37d973e3fa834bdcba464d6c4f041bad867
louzhiyuan <34015181+louzhiyuan@users.noreply.github.com>
2017-12-28 10:06:14 +0800

    Update FactoryConfig.java

--- src/main/java/com/example/demo/common/FactoryConfig.java
+++ src/main/java/com/example/demo/common/FactoryConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
-
+//springboot启动类
 @Configuration
 @EnableAutoConfiguration
 public class FactoryConfig {
